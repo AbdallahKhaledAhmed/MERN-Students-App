dbe5c323819eab1500e3fcdbdb249eaa50e14f1b
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-03-26 03:09:53 +0200

    added key (random) to the inputFields to trigger rerender everytime it gets displayed

--- Frontend/src/componants/InputFields.tsx
+++ Frontend/src/componants/InputFields.tsx
@@ -1,37 +1,40 @@
 import { formData } from "../main";
 
 export default function InputFields({ form, obj }: formData) {
-  obj = obj ?? { name: "", age: undefined, email: "", class: "" };
-
+  const objToShow = obj ?? { name: "", age: undefined, email: "", class: "" };
   return (
-    <form className="flex flex-col gap-0.5 dark:bg-dark-900" ref={form}>
+    <form
+      className="flex flex-col gap-0.5 dark:bg-dark-900"
+      ref={form}
+      key={Math.random()}
+    >
       <input
         name="name"
         type="text"
         placeholder="Name"
         className="form-input"
-        defaultValue={obj.name}
+        defaultValue={objToShow.name}
       />
       <input
         name="age"
         type="number"
         placeholder="Age"
         className="form-input"
-        defaultValue={obj.age !== undefined ? obj.age : ""}
+        defaultValue={objToShow.age !== undefined ? objToShow.age : ""}
       />
       <input
         name="email"
         type="text"
         placeholder="Email"
         className="form-input"
-        defaultValue={obj.email}
+        defaultValue={objToShow.email}
       />
       <input
         name="class"
         type="text"
         placeholder="Class"
         className="form-input"
-        defaultValue={obj.class}
+        defaultValue={objToShow.class}
       />
     </form>
   );
